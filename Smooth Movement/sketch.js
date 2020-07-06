a26a636b0215ce2f6f34bb8a4cacc78fad6633db
quinten.lenaerts <quinten.lenaerts@outlook.be>
2020-07-06 19:15:09 +0200

    refactored Convex Hull, Smooth Movemnt, Sphere Tracing

--- Smooth Movement/sketch.js
+++ Smooth Movement/sketch.js
@@ -1,73 +1,44 @@
-
-// our mover objects
-let m;
-
-// should the path be shown?
-let showPath = false;
+let mover;
 
 function setup() {
 	createCanvas(600, 400)
 
-	// a low speed will result in a very smooth moving object
-	m = new Mover(0.002)
-
-	// generate a 1000 x and y map positions
-	m.generateCharts(0, width, 0, height, 1000);
-
-	// when the mover is done we want to generate the charts again 
-	m.onDone = function() {m.generateCharts(0, width, 0, height, 1000);}
+	mover = new Mover(0.002)
+	mover.moveWithinBounds(0, width, 0, height, 1000);
+	mover.onDone = function() {mover.moveWithinBounds(0, width, 0, height, 1000);}
 
 }
-
 
 function draw() {
 	background(51)
-	let msg = "Press any key to enable path."
 
-	// drawing the path
-	if (showPath) {m.drawPath(); msg = "Press the mouse to disable path."}
+	mover.draw()
+	mover.update()
 
-	// showing our mover object by drawing a small circle at it's position
-	m.draw()
-
-	// updating it's x and y position
-	m.update()
-
-	// listening to events
-	if (keyIsPressed) {showPath = true}
-	if (mouseIsPressed) {showPath = false}
-
-	// displaying info
 	textSize(24)
-	text(msg, 0, height - 10)
-	
-
+	noStroke()
+	fill(255)
+	if (mover.showPath) { text("Press the mouse to disable path.", 0, height - 10) } 
+	else { text("Press any key to enable path.", 0, height - 10) }
 }
 
-
-function Mover(speed) {
-
-	// the current position
+function Mover(speed=0.002) {
 	this.x = 0;
 	this.y = 0;
 
 	// the positions the mover should have in the future
 	this.xChart = []
 	this.yChart = []
-
-	// at what position are we?
 	this.chartIndex = 0;
 
-	// the difference of the perlin noise generation => smaller values => smaller changes
+	// the difference in the perlin noise generation => smaller values => smaller changes
 	this.speed = speed;
-
-	// will be called when the mover is done
+	this.showPath = false; 
 	this.onDone = function() {console.log("Mover is done moving.")}
 
 	// generates the position charts
-	this.generateCharts = function(xMin, xMax, yMin, yMax, n) {
+	this.moveWithinBounds = function(xMin, xMax, yMin, yMax, n) {
 
-		// reseting all the data
 		this.xChart = []
 		this.yChart = []
 		this.chartIndex = 0;
@@ -92,45 +63,32 @@ function Mover(speed) {
 
 	}	
 
-	// set the next position
 	this.update = function() {
 		
+		if (keyIsPressed) this.showPath = true
+		if (mouseIsPressed) this.showPath = false
+		
+		this.chartIndex++;
+
 		if (this.chartIndex < this.xChart.length) {
 
 			this.x = this.xChart[this.chartIndex]
 			this.y = this.yChart[this.chartIndex]
 
-		} else {
+		} else this.onDone() 
 
-			this.onDone()
-
-		}
-
-		// going to the new index
-		this.chartIndex++;
-		
 	}
 
-	// showing the mover
 	this.draw = function() {
-
 		noStroke()
 		fill(255)
 		ellipse(this.x, this.y, 14, 14)
-
+		if (this.showPath) this.drawPath()
 	}
 
-	// draws the path of this mover
 	this.drawPath = function() {
-
 		stroke(0,255,0)
 		noFill()
-
-		for (let i = 0; i < this.xChart.length - 1; i++) {
-
-			line(this.xChart[i], this.yChart[i],this.xChart[i+1], this.yChart[i+1])
-
-		}
-
+		for (let i = 0; i < this.xChart.length - 1; i++) line(this.xChart[i], this.yChart[i],this.xChart[i+1], this.yChart[i+1])
 	}
 }
